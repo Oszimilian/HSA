509879f60a338c1dd46674d28bcfc4aa4d3eddfc
oszi <oszi@whiskey.informatik.hs-augsburg.de>
2022-10-14 08:29:16 +0200

    Praktikum 1 add new project

---
+++ Praktikum01/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 14.10.2022
+ *      Author: oszi
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+	printf("Hallo Welt 001 \n");
+
+	return EXIT_SUCCESS;
+}
